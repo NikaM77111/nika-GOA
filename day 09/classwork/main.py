685765729952f972014876fd881134ec4aa0ea35
NikaM77111 <nikamachavariani771@gmail.com>
2025-03-13 20:31:56 +0100

    commit

---
+++ day 09/classwork/main.py
@@ -0,0 +1,7 @@
+sum = 0  # Initialize the variable sum
+
+# for loop that runs 10 times
+for i in range(10):  # Loops through numbers from 0 to 10
+    sum += i  # Adds each number to the sum
+
+print(sum)  # Prints the sum
